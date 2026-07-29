7231ee104c1297cfb9bf646927763bc8e46e111d
IgorEustachio <igor.daniel.eustachio@gmail.com>
2026-07-28 21:56:06 -0300

    test: add conftest fixtures and health check test

---
+++ stanza_api/tests/test_app.py
@@ -0,0 +1,8 @@
+from http import HTTPStatus
+
+
+def test_health(client):
+    response = client.get('/health')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'status': 'OK'}
