a1dde2547faad473ba2d82d77fb747acf63f2f6e
joao-s25 <joao25as@outlook.com>
2026-08-07 17:12:12 -0300

    frontend: edit (in progress) web buttons

--- frontend/edit.php
+++ frontend/edit.php
@@ -11,6 +11,59 @@
 
 
 
+<!-- Barra Web -->
+
+<div class="bar"> 
+    <a href="home.php"> < </a>    
+
+    <div class="img"></div>
+
+
+    <h2>Categoria<h2>
+        <!-- faça a categoria original vir marcada padrão-->
+        <select id="category" name="category">
+            <option value="">Categoria</option>
+            <option value="book">Livro</option>
+            <option value="poetry">Poesia</option>
+            <option value="story">Conto</option>
+        </select>
+
+
+    <h2>Idioma<h2>
+        <select id="language" name="langugage">
+            <option value="">Idioma</option>
+            <option value="enus">English</option>
+            <option value="ptbr">Português</option>
+        </select>
+
+
+
+
+    <h2>Visibilidade</h2>
+   
+    <div class="visibt">
+
+        <input type="radio" name="visibility" id="public">
+        <label for="public">Público</label>
+
+        <input type="radio" name="visibility" id="private">
+        <label for="private">Privado</label>
+
+    </div>
+
+
+
+</div> 
+
+
+
+
+
+
+
+
+
+
  <div class="mbbt1">
     <button><</button>
     <a href="DPS vc muda">Salvar</a>
@@ -20,17 +73,19 @@
 
 
     <form action="  ALGUMA ACAO  ">
-    <label for="content">Escrever</label>
-
+    <label style="display:none;" for="content">Escrever</label>
     <textarea id="content" name="content"></textarea>
+
+    <div class="mbbt2">
+    
+    <label>12 palavras</label>
+    <label for="fontsize">Tamanho da fonte: </label>                                                
+    <input type="number" id="fontsize" name="fontsize" min="1" max="40">
+    </div>
+
 
     </form>
 
-
-
-    <div class="mbbt2">
-
-</div>
 
 </body>
 </html>
