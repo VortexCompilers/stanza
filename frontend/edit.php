75e27e5d54545ea27f7eeb76592b8b464663ef8c
joao-s25 <joao25as@outlook.com>
2026-08-07 09:06:09 -0300

    frontend: edit (in progress)

---
+++ frontend/edit.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/edit.css">
+
+    <title>Stanza</title>
+</head>
+<body>
+
+
+
+ <div class="mbbt1">
+    <button><</button>
+    <a href="DPS vc muda">Salvar</a>
+    <button>⋮</button>
+</div>
+
+
+
+    <form action="  ALGUMA ACAO  ">
+    <p><label for="content">Escrever</label></p>
+
+    <textarea id="content" name="content"></textarea>
+
+    </form>
+
+
+
+
+</body>
+</html>
