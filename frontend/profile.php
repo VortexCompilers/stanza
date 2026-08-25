2d4179e8d66740b33172cea61bd1e43d63db42ff
joao-s25 <joao25as@outlook.com>
2026-08-25 16:04:18 -0300

    frontend: profile

---
+++ frontend/profile.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/profile.css">
+  <link rel="stylesheet" href="css/header.css">
+
+
+    <title>Stanza</title>
+</head>
+<body>
+
+
+<?php include 'header.php'; ?>
+
+
+
+
+<main>
+
+          <div class="topbar">
+
+            <div class="topinf">
+                <div class="pfp"></div>
+
+                    <div class="userinfo">
+                        <h1> O Fulano de Tal</h1>
+                        <h2>Ingressou em Setembro de 2001</h2>
+                  </div>
+
+              </div>
+
+
+            <div class="botinf">
+                <div class="nbinfo">
+                  <span>544</span>
+                  <p>visualizações</p>
+                </div>
+                
+                <div class="nbinfo">
+                <span>23</span> 
+                  <p>textos escritos</p>
+                </div>
+                
+                <div class="nbinfo">
+                  <span>45</span> 
+                  <p>textos salvos</p>
+                </div>
+
+
+            </div>
+
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <section>
+                    <div class="post">
+                        <div class="img"></div>
+                        <div class="info">
+                            <a href="LINNNNK">Title Example Like This One</a>
+                            <h2>Book</h2>
+                          <div class="postfooter">  
+                            <h3>232 views</h3>
+                            <button>&#9661</button>
+                          </div>
+                        </div>
+                    </div>
+
+                    
+                    <div class="post">
+                        <div class="img"></div>
+                        <div class="info">
+                            <a href="LINNNNK">Title Example Like This One</a>
+                            <h2>Book</h2>
+                          <div class="postfooter">  
+                            <h3>232 views</h3>
+                            <button>&#9661</button>
+                          </div>
+                        </div>
+                    </div>
+
+                    
+                    <div class="post">
+                        <div class="img"></div>
+                        <div class="info">
+                            <a href="LINNNNK">Title Example Like This One</a>
+                            <h2>Book</h2>
+                          <div class="postfooter">  
+                            <h3>232 views</h3>
+                            <button>&#9661</button>
+                          </div>
+                        </div>
+                    </div>
+
+
+                    
+                    <div class="post">
+                        <div class="img"></div>
+                        <div class="info">
+                            <a href="LINNNNK">Title Example Like This One</a>
+                            <h2>Book</h2>
+                          <div class="postfooter">  
+                            <h3>232 views</h3>
+                            <button>&#9661</button>
+                          </div>
+                        </div>
+                    </div>
+
+
+                    
+                    <div class="post">
+                        <div class="img"></div>
+                        <div class="info">
+                            <a href="LINNNNK">Title Example Like This One</a>
+                            <h2>Book</h2>
+                          <div class="postfooter">  
+                            <h3>232 views</h3>
+                            <button>&#9661</button>
+                          </div>
+                        </div>
+                    </div>
+
+
+
+                    
+                    <div class="post">
+                        <div class="img"></div>
+                        <div class="info">
+                            <a href="LINNNNK">Title Example Like This One</a>
+                            <h2>Book</h2>
+                          <div class="postfooter">  
+                            <h3>232 views</h3>
+                            <button>&#9661</button>
+                          </div>
+                        </div>
+                    </div>
+
+
+
+                    
+                    <div class="post">
+                        <div class="img"></div>
+                        <div class="info">
+                            <a href="LINNNNK">Title Example Like This One</a>
+                            <h2>Book</h2>
+                          <div class="postfooter">  
+                            <h3>232 views</h3>
+                            <button>&#9661</button>
+                          </div>
+                        </div>
+                    </div>
+
+
+
+                    
+                    <div class="post">
+                        <div class="img"></div>
+                        <div class="info">
+                            <a href="LINNNNK">Title Example Like This One</a>
+                            <h2>Book</h2>
+                          <div class="postfooter">  
+                            <h3>232 views</h3>
+                            <button>&#9661</button>
+                          </div>
+                        </div>
+                    </div>
+
+
+
+
+                    
+                    <div class="post">
+                        <div class="img"></div>
+                        <div class="info">
+                            <a href="LINNNNK">Title Example Like This One</a>
+                            <h2>Book</h2>
+                          <div class="postfooter">  
+                            <h3>232 views</h3>
+                            <button>&#9661</button>
+                          </div>
+                        </div>
+                    </div>
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</main>
+
+
+</body>
+</html>
